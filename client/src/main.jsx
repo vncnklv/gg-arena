01cabf663b37b3255c4b78209c98a7ae31713524
vncnklv <vncnklv0@gmail.com>
2025-03-13 19:36:39 +0200

    Updated the fonts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,6 +10,6 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <StrictMode>
             <App />
-        </StrictMode>,
+        </StrictMode>
     </BrowserRouter>
 )
