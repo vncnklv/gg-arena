a6755377038587fba5ed93d0a2c423af5e6af6e1
vncnklv <vncnklv0@gmail.com>
2025-03-13 23:51:23 +0200

    Created the leaderboard layout.

--- client/src/components/leaderboard/Leaderboard.jsx
+++ client/src/components/leaderboard/Leaderboard.jsx
@@ -1,6 +1,78 @@
+import trophy from '/trophy.png'
+
+import styles from './Leaderboard.module.css'
+
 function Leaderboard() {
     return (
-        <h1>Leaderboard</h1>
+        <div className="container">
+            <table className={styles['tournaments-table']}>
+                <thead className={styles['table-header']}>
+                    <tr className={styles['table-row']}>
+                        <th></th>
+                        <th>Player</th>
+                        <th>Last played game</th>
+                        <th>Wins</th>
+                        <th>Trophies</th>
+                    </tr>
+                </thead>
+                <tbody className={styles['table-body']}>
+                    <tr className={styles['table-row']}>
+                        <td className={styles['position-cell']}>01</td>
+                        <td className={styles['player-name-cell']}>
+                            <p>Player Player</p>
+                            <p>@player</p>
+                        </td>
+                        <td>Counter Strike</td>
+                        <td>25</td>
+                        <td className={styles['trophies-cell']}>
+                            <p>100</p>
+                            <img src={trophy} alt="Trophy Symbol" />
+                        </td>
+                    </tr>
+                    <tr className={styles['table-row']}>
+                        <td className={styles['position-cell']}>02</td>
+                        <td className={styles['player-name-cell']}>
+                            <p>Player Player</p>
+                            <p>@player</p>
+                        </td>
+                        <td>Counter Strike</td>
+                        <td>25</td>
+                        <td className={styles['trophies-cell']}>
+                            <p>100</p>
+                            <img src={trophy} alt="Trophy Symbol" />
+                        </td>
+                    </tr>
+                    <tr className={styles['table-row']}>
+                        <td className={styles['position-cell']}>03</td>
+                        <td className={styles['player-name-cell']}>
+                            <p>Player Player</p>
+                            <p>@player</p>
+                        </td>
+                        <td>Counter Strike</td>
+                        <td>25</td>
+                        <td className={styles['trophies-cell']}>
+                            <p>100</p>
+                            <img src={trophy} alt="Trophy Symbol" />
+                        </td>
+                    </tr>
+                    <tr className={styles['table-row']}>
+                        <td className={styles['position-cell']}>04</td>
+                        {/* Todo: Link to user profile */}
+                        <td className={styles['player-name-cell']}>
+                            <p>Player Player</p>
+                            <p>@player</p>
+                        </td>
+                        {/* Todo: Link to the game */}
+                        <td>Counter Strike</td>
+                        <td>25</td>
+                        <td className={styles['trophies-cell']}>
+                            <p>100</p>
+                            <img src={trophy} alt="Trophy Symbol" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     );
 }
 
