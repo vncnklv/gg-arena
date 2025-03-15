f9ce947c87f12f74eac3bb996a20621794bdf75c
vncnklv <vncnklv0@gmail.com>
2025-03-15 12:20:34 +0200

    Create login page layout

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,6 +1,21 @@
+import { Link } from 'react-router'
+
+import styles from './Login.module.css'
+
 function Login() {
     return (
-        <h1>Login</h1>
+        <div className="container">
+            <div className={styles['login-container']}>
+                <form className={styles['login-form']}>
+                    <label htmlFor="username" className={styles.label}>Username</label>
+                    <input type="text" name="username" className={styles['text-input']} />
+                    <label htmlFor="password" className={styles.label}>Password</label>
+                    <input type="password" name="password" className={styles['text-input']} />
+                    <input type="submit" value='login' className={styles['submit-button']} />
+                </form>
+                <p className={styles['register-redirect']}>Do not have an account? Create one <Link to='/register'>here</Link>.</p>
+            </div>
+        </div>
     );
 }
 
