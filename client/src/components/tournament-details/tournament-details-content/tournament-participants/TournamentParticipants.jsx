0605f2edd2d6a5bd570df1252a9e346372767172
vncnklv <vncnklv0@gmail.com>
2025-03-29 22:54:28 +0200

    Visualised participants in tournament page

--- client/src/components/tournament-details/tournament-details-content/tournament-participants/TournamentParticipants.jsx
+++ client/src/components/tournament-details/tournament-details-content/tournament-participants/TournamentParticipants.jsx
@@ -1,6 +1,29 @@
+import { useEffect } from "react";
+import { useOutletContext } from "react-router";
+
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import styles from './TournamentParticipants.module.css';
+
 function TournamentParticipants() {
+    const { participants, participantsRefetch } = useOutletContext();
+
+    useEffect(() => {
+        participantsRefetch();
+    }, []);
+
     return (
-        <div>participants</div>
+        <div className={styles['participants-wrapper']}>
+            {participants.map(p => (
+                <div className={styles['participant']}>
+                    <div className={styles['icon']}>
+                        <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <p className={styles['participant-username']}>{p.user.email}</p>
+                </div>
+            ))}
+        </div>
     );
 }
 
