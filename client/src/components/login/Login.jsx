dc293398f273411551f78446cfb5fa6a63996f68
vncnklv <vncnklv0@gmail.com>
2025-04-01 22:21:42 +0300

    Add react-form-hook and refactored Login component

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,63 +1,56 @@
 import { Link, useNavigate } from 'react-router'
-import { useState } from 'react';
 
 import { useAuth } from '../../providers/UserProvider';
+import { useForm } from 'react-hook-form';
 
 import styles from './Login.module.css'
+import useLogin from '../../api/useLogin';
 
 function Login() {
-    const [userData, setUserData] = useState({
-        email: '',
-        password: ''
-    });
-    const [error, setError] = useState('');
-
+    const { register, handleSubmit, formState: { errors } , setError, clearErrors} = useForm();
     const navigate = useNavigate();
-    const { login, isLoading } = useAuth();
+    const [login, loginIsLoading, loginError] = useLogin();
 
-    const changeHandler = (e) => {
-        setUserData(oldData => {
-            const newData = { ...oldData }
-            newData[e.target.name] = e.target.value
-            return newData;
-        });
-    }
-
-    const submitHandler = async (e) => {
-        e.preventDefault();
-
-        if (!userData.email && !userData.password) {
-            setError("Email and username not provided!");
-            return;
-        }
-        if (!userData.email) {
-            setError("Email not provided");
-            return;
-        }
-
-        if (!userData.password) {
-            setError("Password not provided");
-            return;
-        }
-
-        try {
-            await login(userData.email, userData.password);
-            navigate("/");
-        } catch (error) {
-            setError(error.message);
-        }
+    const submitHandler = async (userData) => {
+        await login(userData);
+        navigate("/");
     }
 
     return (
         <div className="container">
             <div className={styles['login-container']}>
-                <form onSubmit={submitHandler} className={styles['login-form']}>
-                    <label htmlFor="email" className={styles.label}>Email</label>
-                    <input id="email" type="text" name="email" className={styles['text-input']} onChange={changeHandler} value={userData.email} />
-                    <label htmlFor="password" className={styles.label}>Password</label>
-                    <input id="password" type="password" name="password" className={styles['text-input']} onChange={changeHandler} value={userData.password} />
-                    {error && <p className={styles['error-message']}>{error}</p>}
-                    <input type="submit" value='login' className={styles['submit-button']} disabled={isLoading}/>
+                <form onSubmit={handleSubmit(submitHandler)} className={styles['login-form']}>
+                    <label htmlFor="email" className={styles.label}>
+                        <span>Email</span>
+                        {errors.email && <span className={styles['input-error']}>{errors.email.message}</span>}
+                    </label>
+                    <input
+                        id="email"
+                        type="text"
+                        className={styles['text-input']}
+                        {...register('email', {
+                            required: "Email is required.",
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                                message: 'Invalid email adderss.'
+                            }
+                        })}
+                    />
+
+                    <label htmlFor="password" className={styles.label}>
+                        <span>Password</span>
+                        {errors.password && <span className={styles['input-error']}>{errors.password.message}</span>}
+                    </label>
+                    <input
+                        id="password"
+                        type="password"
+                        className={styles['text-input']}
+                        {...register('password', { required: "Password is required." })}
+                    />
+
+                    {loginError && <p className={styles['error-message']}>{loginError}</p>}
+                    
+                    <input type="submit" value='login' className={styles['submit-button']} disabled={loginIsLoading} onClick={() => {clearErrors()}}/>
                 </form>
                 <p className={styles['register-redirect']}>Do not have an account? Create one <Link to='/register'>here</Link>.</p>
             </div>
