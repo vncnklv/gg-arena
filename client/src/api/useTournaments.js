bb5e05d809dafb836bbe5f0952fbe8e71a548f34
vncnklv <vncnklv0@gmail.com>
2025-04-02 23:40:09 +0300

    Adjusted the useTournament hook to use pagination

--- client/src/api/useTournaments.js
+++ client/src/api/useTournaments.js
@@ -1,28 +1,24 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
-import { useParams } from "react-router";
 
-export default function useTournaments(gameId, status, searchTerm) {
-    const [path, setPath] = useState(`/data/tournaments?where=gameId LIKE "${gameId}"${searchTerm ? ` AND name LIKE "${searchTerm}"` : ''}`);
+export default function useTournaments(pageSize = 3, page = 1, gameId = null, status = 'upcoming', searchTerm = '') {
+    const [path, setPath] = useState('');
     const [data, isLoading, error] = useFetch(path, []);
-
-
 
     useEffect(() => {
         let statusFilter;
         if (status == 'ongoing') {
-            statusFilter = ` AND startDate < ${Date.now()} AND endDate > ${Date.now()}`;
+            statusFilter = `startDate < ${Date.now()} AND endDate > ${Date.now()}`;
         }
         else if (status == 'completed') {
-            statusFilter = ` AND endDate < ${Date.now()}`;
+            statusFilter = `endDate < ${Date.now()}`;
         }
         else {
-            statusFilter = ` AND startDate > ${Date.now()}`;
+            statusFilter = `startDate > ${Date.now()}`;
         }
 
-        setPath(`/data/tournaments?where=gameId LIKE "${gameId}"${searchTerm ? ` AND name LIKE "${searchTerm}"` : ''}${statusFilter}`);
+        setPath(`/data/tournaments?where=${statusFilter}${searchTerm ? ` AND name LIKE "${searchTerm}"` : ''}${gameId ? ` AND gameId LIKE "${gameId}"` : ''}&offset=${pageSize * (page - 1)}&pageSize=${pageSize}`);
     }, [searchTerm, status]);
-
 
     return [data, isLoading, error];
 }
